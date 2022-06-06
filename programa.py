5736fa2bbb1f9fa7e64863779dd6b66015546802
Lucas Dani <lucasdani@gmail.com>
2022-06-06 21:47:49 +0000

    tercer commit

--- programa.py
+++ programa.py
@@ -1,9 +1,11 @@
 import ing2i as ing2i
 import ing2s as ing2s
+import suma as s
 print("Bienvenidos al programa")
 a=0
 b=0
 s1=""
 s2=""
 ing2i.ing2i(a,b)
-ing2s.ing2s(s1,s2)
+#ing2s.ing2s(s1,s2)
+print("suma=",s.suma(a,b))
