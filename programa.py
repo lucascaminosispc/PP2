35218675397303dee82ab8a56501a22f59113dcf
Lucas Caminos <lucascaminosispc@gmail.com>
2022-06-07 18:08:47 -0300

    commit

--- programa.py
+++ programa.py
@@ -2,6 +2,7 @@ import ing2i as ing2i
 import ing2s as ing2s
 import suma as s
 import fun1 as fun1
+import f2 as f2
 print("Bienvenidos al programa")
 a=0
 b=0
@@ -9,5 +10,6 @@ s1=""
 s2=""
 ing2i.ing2i(a,b)
 #ing2s.ing2s(s1,s2)
-print("suma=",s.suma(a,b))
-print("fun1=", fun1.fun1(a,b))
+#print("suma=",s.suma(a,b))
+#print("fun1=", fun1.fun1(a,b))
+print("F2=", f2.f2(a,b))
