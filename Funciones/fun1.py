d51bd1ac48d52a9b80d4438fdafeb848a70d3dc1
Lucas Caminos <105889826+lucascaminosispc@users.noreply.github.com>
2022-06-09 11:37:23 -0300

    fun1

--- Funciones/fun1.py
+++ Funciones/fun1.py
@@ -1,2 +1,5 @@
-def fun1(a,b):
+def fun1():
+  print("\n*****Función ing2i****\n")
+  a=int(input("a= "))
+  b=int(input("b= "))
   return a*b
--- programa.py
+++ programa.py
@@ -2,11 +2,12 @@
 from Funciones.ing2i import ing2i
 from Funciones.ing2s import ing2s
 from Funciones.suma import suma
+from Funciones.fun1 import fun1
 from Funciones.f2 import f2
 
 print("Bienvenidos al programa")
 ing2i()
 ing2s()
 print("suma=",suma())
-#print("fun1=", fun1.fun1(a,b))
+print("fun1=",fun1())
 #print("F2=", f2.f2())
